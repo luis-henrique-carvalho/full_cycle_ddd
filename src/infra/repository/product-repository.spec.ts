0868f165eaec7b6d2c9d08404178db5cde3475aa
luis-henrique-carvalho <luisdev9225@gmail.com>
2025-04-09 00:44:17 -0300

    Add Sequelize integration with Product model

---
+++ src/infra/repository/product-repository.spec.ts
@@ -0,0 +1,17 @@
+import { Sequelize } from "sequelize-typescript";
+describe("Product repository tests", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    afterEach(async () => {
+      await sequelize.close();
+    });
+  });
+});
